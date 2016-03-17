6d32b2406bb18d0743a9403ed1275b4975cd77ce
Kyle Stengline <kyle.stengline@gmail.com>
2016-03-16 20:12:00 -0400

    adds html, css and links

---
+++ Gemfile
@@ -0,0 +1,13 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+gem "rails"
+gem "rake"
+gem "pg"
+gem "bcrypt"
+gem "sinatra"
+gem "sinatra-contrib"
+gem "pry"
+gem "rspec"
+gem "redcarpet"
+gem "capybara"
---
+++ server.rb
@@ -0,0 +1,18 @@
+require "sinatra/base"
+require "pry"
+require "rails"
+
+module Portfolio
+	class Server < Sinatra::Base
+
+		get "/" do 
+			erb :index
+		end
+
+		get "/contact" do
+			erb :contact
+		end
+
+
+	end
+end
