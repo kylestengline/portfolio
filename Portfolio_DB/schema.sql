dd5e507af44f4c2c0960c16cdf2aaf01e63549c5
Kyle Stengline <kyle.stengline@gmail.com>
2016-03-21 22:27:03 -0400

    updates schema

--- Portfolio_DB/schema.sql
+++
@@ -1,9 +0,0 @@
--- DROP TABLE contacts CASCADE;
-
--- CREATE TABLE contacts (
--- 	id SERIAL PRIMARY KEY,
--- 	fname VARCHAR (500),
--- 	lname VARCHAR (500),
--- 	email VARCHAR (500),
--- 	message TEXT
--- );
