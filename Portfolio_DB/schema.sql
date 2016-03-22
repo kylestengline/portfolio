403c889a2c881aa47bcf3083fc2db50a21380cc5
Kyle Stengline <kyle.stengline@gmail.com>
2016-03-21 22:17:28 -0400

    updates portfolio

--- Portfolio_DB/schema.sql
+++ Portfolio_DB/schema.sql
@@ -1,9 +1,9 @@
-DROP TABLE IF EXISTS contacts CASCADE;
+-- DROP TABLE IF EXISTS contacts CASCADE;
 
-CREATE TABLE contacts(
-	id SERIAL PRIMARY KEY,
-	fname VARCHAR (500) NOT NULL,
-	lname VARCHAR (500) NOT NULL,
-	email VARCHAR (500) NOT NULL,
-	message TEXT NOT NULL
-);
+-- CREATE TABLE contacts (
+-- 	id SERIAL PRIMARY KEY,
+-- 	fname VARCHAR (500) NOT NULL,
+-- 	lname VARCHAR (500) NOT NULL,
+-- 	email VARCHAR (500) NOT NULL,
+-- 	message TEXT NOT NULL
+-- );
