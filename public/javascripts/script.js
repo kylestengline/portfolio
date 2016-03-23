a62038de6dcffd4de9776136f3e3e4b08543e97b
Kyle Stengline <kyle.stengline@gmail.com>
2016-03-22 23:41:39 -0400

    adds jquery scroll and image for contact

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,55 +3,64 @@
 $(document).ready(function() {
 
 
-// scrolls to the skills area
+// function to scroll to the skills area
+// will not scroll until clicked
 var	scrollToSkills = function(skills) {
-	var skill = $("a[name='" +skills+"']");
-	$('html,body').animate({scrollTop: skill.offset().top},'slow');
+	var skill = $("a[name=" +skills+ "]");
+	$('html,body').animate({scrollTop: skill.offset().top},1500);
 }
 
 	$("#skills").click(function() {
 		scrollToSkills('skills');
+		// makes scroll go smooth when we return false
+			return false;
+
 	})
 
 
 	// scrolls to the projects area
 	var	scrollToProjects = function(projects) {
-		var projects = $("a[name='" +projects+ "']");
-		$('html,body').animate({scrollTop: projects.offset().top},'slow');
+		var projects = $("a[name=" +projects+ "]");
+		$('html,body').animate({scrollTop: projects.offset().top},1500);
 }
 
 	$("#my_projects").click(function() {
 		scrollToProjects('projects');
+			return false;
 	}) 
 
 	$("#my_projects_two").click(function() {
 		scrollToProjects('projects');
+			return false;
 	}) 
 
 
 	// Scrolls to the contact area
 	var	scrollToContact = function(contact) {
-		var contact = $("a[name='" +contact+"']");
-		$('html,body').animate({scrollTop: contact.offset().top},'slow');
+		var contact = $("a[name=" +contact+"]");
+		$('html,body').animate({scrollTop: contact.offset().top},1500);
 }
 
 	$("#contact_me").click(function() {
 		scrollToContact('Contact');
+			return false;
 	}) 
 
 	$("#contact").click(function() {
 		scrollToContact('Contact');
+			return false;
 	})
 
 
 // Scrolls to top of page
 	var scrollToTop = function(top) {
-		var top = $("a[name='" +top+ "']");
-		$('html, body').animate({scrollTop: top.offset().top},'slow');
+		var top = $("a[name=" +top+ "]");
+		$('html, body').animate({scrollTop: top.offset().top},1500);
 	}
 
 	$("#back_to_top").click(function() {
 		scrollToTop('top');
+			return false;
 	})
 
 });
