c80acc029376ae7a6c2984b0add22e74cff1d41b
Kyle Stengline <kyle.stengline@gmail.com>
2016-04-18 17:19:33 -0400

    temporary fix to mobile scroll

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 $(document).ready(function() {
-	$('html, body').scrollTop(0);
+	$(this).scrollTop(0);
 // bio fades in after name appears
 		$('.bio').hide();
 	setTimeout(function(){
