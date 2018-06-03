1fbd63e916b594574943d6ef7a8d5e1831b28eb7
Kyle Stengline <kyle.stengline@gmail.com>
2018-06-03 01:50:29 -0700

    setup scroll to projects

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,13 +3,10 @@
 $(document).ready(function() {
 
 	// scrolls to the projects area
-	var	scrollToProjects = function(projects) {
-		var projects = $("a[name=" +projects+ "]");
-		$('html,body').animate({scrollTop: projects.offset().top},1500);
-  }
-
-	$("#my_projects").click(function() {
-		scrollToProjects('projects');
-    return false;
-	}) 
+  $("#my_projects").click(function() {
+    var scrollToProj = $('#scripts').scrollLeft();
+    $("html, body").animate({
+      scrollLeft: scrollToProj.offset().left}, 1500);
+    event.preventDefault();
+  })
 });
